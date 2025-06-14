a97269ba32250d00dbc6e1bce65ea71700f8f865
Darin Verduzco <dlverduzco@ucdavis.edu>
2025-06-13 17:51:38 -0700

    update main import
    update main import

--- code/main.py
+++ code/main.py
@@ -7,13 +7,6 @@ from dotenv import load_dotenv # For loading environment variables
 from utils import load_yaml_config # For loading YAML configurations
 from utils import initialize_db, insert_publications # Import functions from utils
 from utils import load_all_publications # Import the data loading utility
-
-ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-if ROOT_DIR not in sys.path:
-    sys.path.insert(0, ROOT_DIR)
-
-from paths import VECTOR_DB_DIR
-
 from paths import VECTOR_DB_DIR # Import the path constant
 from utils import select_prompt_by_similarity
 from llm_service import respond_to_query # For handling RAG responses
